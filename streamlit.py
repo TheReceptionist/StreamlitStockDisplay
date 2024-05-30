7b1f2515bb4f26ebcbe7711a931257aceb1992d6
firefoxfirefox <spittingverse@gmail.com>
2024-05-29 22:05:45 -0700

    use yahoo finance

--- streamlit.py
+++ streamlit.py
@@ -1,20 +1,31 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import yfinance as yf
 import matplotlib.pyplot as plt
-time_intervals = 30
+def tickerListToDataFrame(tickers, timeframe):
+    df_list = []
+    df = pd.DataFrame()
+    for ticker in tickers:
+        data = yf.download(ticker, period=f'{timeframe}', group_by='column')
+        data = data.get('Open').to_frame()
+        data = data.rename(columns={'Open':f'{ticker}'})
+        df_list.append(data)
+    df = pd.concat(df_list, axis=1).fillna(0)
+    return df
 st.title('Display of stock prices of different portfolios')
-portfolios = {'Portfolio 1':['Google', 'Apple', 'Versa', 'Uber', 'Lyft', 'Boeing'],
-             'Portfolio 2':['Netflix', 'Twitter', 'Facebook'],
-             'Portfolio 3':['Netflix', 'Twitter', 'Facebook', 'Yahoo']}
-
+timeframes = ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+tf = st.selectbox('Select timeframe to display:', timeframes)
+portfolios = {'Portfolio 1':[['GOOG', 'AAPL', 'UBER', 'LYFT', 'BA']],
+             'Portfolio 2':[['A', 'OKTA', 'UHAL']],
+             'Portfolio 3':[['NFLX', 'META', 'TSLA', 'DELL']]}
+for p in portfolios.values():
+    p.append(tickerListToDataFrame(p[0], tf))
 portfol = st.selectbox('Select stock portfolio to display:', portfolios)
-#randomly generate stock prices
-values = np.random.randint(100,1000,(time_intervals,len(portfolios[portfol])))
-stocks_chart = pd.DataFrame(data=values, columns=portfolios[portfol])
-st.line_chart(stocks_chart)
-st.bar_chart(stocks_chart)
-st.title('Pie chart showing % of total stock price on a given date')
-date = st.slider('Select date for pie chart', min_value = 0, max_value = time_intervals-1 )
-plt.pie(values[date], labels=portfolios[portfol], autopct = '%.0f%%')
+st.line_chart(portfolios[portfol][1])
+st.bar_chart(portfolios[portfol][1])
+date = next(iter(portfolios[portfol][1].index))
+if tf != '1d':
+    date = st.select_slider('Select date for pie chart', portfolios[portfol][1].index)
+plt.pie(portfolios[portfol][1].loc[date], labels=portfolios[portfol][0], autopct = '%.0f%%')
 st.pyplot(plt)
